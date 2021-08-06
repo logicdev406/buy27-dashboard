70b77401b34b45220bb963fb939ecce3d8e96974
patrick sunday <sundayblinkz1@gmail.com>
2021-08-07 00:16:34 +0100

    created get total ernings and orders count actions g

--- redux/actions/productAction.js
+++ redux/actions/productAction.js
@@ -54,3 +54,55 @@ export const getUsersCount = (token) => async (dispatch) => {
     });
   }
 };
+
+export const getOrdersCount = (token) => async (dispatch) => {
+  try {
+    if (token) {
+      setAuthToken(token);
+    }
+    dispatch({ type: t.GET_ORDERS_COUNT_REQUEST });
+
+    const { data } = await axios.get(
+      "https://backend.buy27.ng/api/orders/get/count"
+    );
+
+    dispatch({
+      type: t.GET_ORDERS_COUNT_SUCCESS,
+      payload: data.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: t.GET_ORDERS_COUNT_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const getTotalErnings = (token) => async (dispatch) => {
+  try {
+    if (token) {
+      setAuthToken(token);
+    }
+    dispatch({ type: t.GET_TOTAL_ERNINGS_COUNT_REQUEST });
+
+    const { data } = await axios.get(
+      "https://backend.buy27.ng/api/orders/get/totalsales"
+    );
+
+    dispatch({
+      type: t.GET_TOTAL_ERNINGS_COUNT_SUCCESS,
+      payload: data.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: t.GET_TOTAL_ERNINGS_COUNT_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
